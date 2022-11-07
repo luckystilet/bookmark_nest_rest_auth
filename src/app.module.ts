a43daebbdb676faf402aadd62a5f16fb857c1af0
alex <luckystilet@gmail.com>
2022-11-07 20:35:50 +0200

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+import { BookmarkModule } from './bookmark/bookmark.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
   controllers: [],
-  providers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule {
+}
+
